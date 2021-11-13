fca79804ca94812394a34ff2aafbb513ff827a90
Setant <for.ek.101@gmail.com>
2021-11-13 20:18:56 +0200

    init commit

---
+++ src/com/test.java
@@ -0,0 +1,26 @@
+package com;
+
+public class test {
+    public static void main(String[] args) {
+
+        int beerNum = 99;
+        String word = "бутылок (бутылки)";
+
+        while (beerNum >= 0){
+
+            if (beerNum > 0){
+                if (beerNum ==1){
+                    word = "бутылка";
+                }
+                System.out.println(beerNum+" "+ word + " пива на стене");
+                System.out.println(beerNum+" "+ word + " пива.");
+                System.out.println("Возьми одну.");
+                System.out.println("Пусти по кругу.");
+            } else {
+                System.out.println("Нет бутылок пива на стене");
+            }
+
+            beerNum = beerNum - 1;
+        }
+    }
+}
